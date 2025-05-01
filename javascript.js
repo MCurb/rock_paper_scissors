f2367cdd209ab3aac58e8f2914eb0b792e8b9a3d
MCurb <marcoscurbeco03@gmail.com>
2025-04-30 18:08:43 -0600

    Take the user's input from the clicked button text

--- javascript.js
+++ javascript.js
@@ -10,22 +10,22 @@ function getComputerChoice() {
   }
 }
 
-//Get player choice function
-function getHumanChoice() {
-  const humanChoice = prompt("What's your choice: \nRock, Paper or Scissors?")
-    .toLowerCase()
-    .trim();
-  if (
-    humanChoice === "rock" ||
-    humanChoice === "paper" ||
-    humanChoice === "scissors"
-  ) {
-    return humanChoice;
-  } else {
-    alert("Invalid choice! Please type rock, paper, or scissors.");
-    return getHumanChoice();
-  }
-}
+// //Get player choice function
+// function getHumanChoice() {
+//   const humanChoice = prompt("What's your choice: \nRock, Paper or Scissors?")
+//     .toLowerCase()
+//     .trim();
+//   if (
+//     humanChoice === "rock" ||
+//     humanChoice === "paper" ||
+//     humanChoice === "scissors"
+//   ) {
+//     return humanChoice;
+//   } else {
+//     alert("Invalid choice! Please type rock, paper, or scissors.");
+//     return getHumanChoice();
+//   }
+// }
 
 //Get player and computer choices to play a round
 function playRound(computerChoice, humanChoice) {
@@ -46,46 +46,60 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
+
+const buttons = document.querySelectorAll(".input-btn")
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const userInput = button.textContent
+    const computerChoice = getComputerChoice();
+    playRound(computerChoice, userInput)
+  })
+})
+
+
+
+
 //Create a function that plays the entire game (5 rounds)
 let computerScore = 0;
 let humanScore = 0;
-let currentRound = 0;
+// let currentRound = 0;
 
-function playGame() {
-  //Increment and log the Round number
-  currentRound++;
-  console.log(`ROUND: ${currentRound}`);
+// function playGame() {
+//   //Increment and log the Round number
+//   currentRound++;
+//   console.log(`ROUND: ${currentRound}`);
 
-  //Log the user and computer choices in the round
-  const computerChoice = getComputerChoice();
-  const humanChoice = getHumanChoice();
-  console.log(
-    `Computer Choice: ${computerChoice} \nPlayer Choice: ${humanChoice}\n\n`
-  );
+//   //Log the user and computer choices in the round
+//   const computerChoice = getComputerChoice();
+//   const humanChoice = getHumanChoice();
+//   console.log(
+//     `Computer Choice: ${computerChoice} \nPlayer Choice: ${humanChoice}\n\n`
+//   );
 
-  //Log the round winner and increment the winner score
-  playRound(computerChoice, humanChoice);
+//   //Log the round winner and increment the winner score
+//   playRound(computerChoice, humanChoice);
 
-  //Log the computer and player scores up to the current round
-  console.log(
-    `Computer Score: ${computerScore} \nPlayer Score: ${humanScore}\n\n`
-  );
-}
+//   //Log the computer and player scores up to the current round
+//   console.log(
+//     `Computer Score: ${computerScore} \nPlayer Score: ${humanScore}\n\n`
+//   );
+// }
 
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
 
-//Final result function
-function finalResult() {
-  if (computerScore > humanScore) {
-    console.log("Loser!!! GAME OVER");
-  } else if (computerScore < humanScore) {
-    console.log("You're the winner! (But just for this time;)");
-  } else if (computerScore === humanScore) {
-    console.log("You both had the same luck today!");
-  }
-}
-finalResult();
+// //Final result function
+// function finalResult() {
+//   if (computerScore > humanScore) {
+//     console.log("Loser!!! GAME OVER");
+//   } else if (computerScore < humanScore) {
+//     console.log("You're the winner! (But just for this time;)");
+//   } else if (computerScore === humanScore) {
+//     console.log("You both had the same luck today!");
+//   }
+// }
+// finalResult();
