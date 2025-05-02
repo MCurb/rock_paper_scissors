08fc1de735f8a5e952fb97be8b387e86bee13ed0
MCurb <marcoscurbeco03@gmail.com>
2025-05-01 18:40:48 -0600

    Make everything work and function correctly
    But I need to make the code more readable and shorter

--- javascript.js
+++ javascript.js
@@ -1,117 +1,69 @@
-//Get computer choice function
-function getComputerChoice() {
-  const computerChoice = Math.random();
-  if (computerChoice < 0.33) {
-    return "rock";
-  } else if (computerChoice < 0.66) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
+let computerScore = 0;
+let humanScore = 0;
+let roundCounter = 1;
 
-// //Get player choice function
-// function getHumanChoice() {
-//   const humanChoice = prompt("What's your choice: \nRock, Paper or Scissors?")
-//     .toLowerCase()
-//     .trim();
-//   if (
-//     humanChoice === "rock" ||
-//     humanChoice === "paper" ||
-//     humanChoice === "scissors"
-//   ) {
-//     return humanChoice;
-//   } else {
-//     alert("Invalid choice! Please type rock, paper, or scissors.");
-//     return getHumanChoice();
-//   }
-// }
+const divRound = document.querySelector(".round-winner");
+const divCompScore = document.querySelector(".computer-score");
+const divHumScore = document.querySelector(".human-score");
+const divGameResult = document.querySelector(".game-result")
+const round = document.querySelector(".round-counter")
+const computerCounter = document.querySelector(".computer-counter")
+const humanCounter = document.querySelector(".human-counter")
+
+//Get computer choice function
+const getComputerChoice = (array) => array[Math.floor(Math.random() * array.length)]
+const choices = ["rock", "paper", "scissors"]
+const computerChoice = getComputerChoice(choices)
+
 
 //Get player and computer choices to play a round
 function playRound(computerChoice, humanChoice) {
-  //Add a reference of the div
-  const divRound = document.querySelector(".round-winner")
-  const divCompScore = document.querySelector(".computer-score")
-  const divHumScore = document.querySelector(".human-score")
- 
- 
-  if (
-    (computerChoice === "rock" && humanChoice === "paper") ||
-    (computerChoice === "paper" && humanChoice === "scissors") ||
-    (computerChoice === "scissors" && humanChoice === "rock")
-  ) {
-    humanScore++;
-    divRound.textContent = `You win! ${humanChoice} beats ${computerChoice}. (Score: You ${humanScore} - ${computerScore} Computer)`;
-  } else if (computerChoice === humanChoice) {
-    computerScore++;
-    humanScore++;
-    divRound.textContent = `You both selected ${humanChoice}. It's a tie!. (Score: You ${humanScore} - ${computerScore} Computer)`;
-  } else {
-    computerScore++;
-    divRound.textContent = `You lose! ${computerChoice} beats ${humanChoice}. (Score: You ${humanScore} - ${computerScore} Computer)`;
+  round.textContent = `Round: ${roundCounter}`
+  computerCounter.textContent = `${computerScore}`
+  humanCounter.textContent = `${humanScore}`
+  if (computerScore < 5 && humanScore < 5) {
+    if (
+      (computerChoice === "rock" && humanChoice === "paper") ||
+      (computerChoice === "paper" && humanChoice === "scissors") ||
+      (computerChoice === "scissors" && humanChoice === "rock")
+    ) {
+      humanScore++;
+      roundCounter++;
+      divRound.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else if (computerChoice === humanChoice) {
+      roundCounter++
+      computerScore++;
+      humanScore++;
+      divRound.textContent = `You both selected ${humanChoice}. It's a tie!.`;
+    } else {
+      roundCounter++;
+      computerScore++;
+      divRound.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+  } else if (computerScore === 5 || humanScore === 5) {
+    divRound.textContent = finalResult();
   }
+  divRound.setAttribute("style", "color: white")
 }
 
-  
-
-
-const buttons = document.querySelectorAll(".input-btn")
+const buttons = document.querySelectorAll(".input-btn");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (computerScore < 5 && humanScore < 5) {
-    const userInput = button.textContent
-    const computerChoice = getComputerChoice();
-    playRound(computerChoice, userInput)
-    }
-    }
-    
-  )
-})
+    if (computerScore < 6 && humanScore < 6) {
+      const userInput = button.textContent;
+      const computerChoice = getComputerChoice(choices);
+      playRound(computerChoice, userInput);
+    } 
+  });
+});
 
-
-
-
-//Create a function that plays the entire game (5 rounds)
-let computerScore = 0;
-let humanScore = 0;
-// let currentRound = 0;
-
-// function playGame() {
-//   //Increment and log the Round number
-//   currentRound++;
-//   console.log(`ROUND: ${currentRound}`);
-
-//   //Log the user and computer choices in the round
-//   const computerChoice = getComputerChoice();
-//   const humanChoice = getHumanChoice();
-//   console.log(
-//     `Computer Choice: ${computerChoice} \nPlayer Choice: ${humanChoice}\n\n`
-//   );
-
-//   //Log the round winner and increment the winner score
-//   playRound(computerChoice, humanChoice);
-
-//   //Log the computer and player scores up to the current round
-//   console.log(
-//     `Computer Score: ${computerScore} \nPlayer Score: ${humanScore}\n\n`
-//   );
-// }
-
-// playGame();
-// playGame();
-// playGame();
-// playGame();
-// playGame();
-
-// //Final result function
-// function finalResult() {
-//   if (computerScore > humanScore) {
-//     console.log("Loser!!! GAME OVER");
-//   } else if (computerScore < humanScore) {
-//     console.log("You're the winner! (But just for this time;)");
-//   } else if (computerScore === humanScore) {
-//     console.log("You both had the same luck today!");
-//   }
-// }
-// finalResult();
+function finalResult() {
+  if (computerScore > humanScore) {
+    return "Loser!!! GAME OVER";
+  } else if (computerScore < humanScore) {
+    return "You're the winner! (But just for this time;)";
+  } else if (computerScore === humanScore) {
+    return "You both had the same luck today!";
+  }
+}
